2283a8a1c20f0cdab260dc0a00e457f9e0f5b68b
Calvin000 <hetes.calvin@gmail.com>
2019-01-12 16:41:09 -0500

    Histogram

--- vision_a.py
+++ vision_a.py
@@ -1,17 +1,24 @@
 import cv2
 import numpy as np
+import sys
 from time import sleep
+from matplotlib import pyplot as plt
 
 def nothing(x):pass
 
-camera = cv2.VideoCapture(1)
-cv2.namedWindow('videoUI', cv2.WINDOW_NORMAL)
-cv2.createTrackbar('T', 'videoUI', 0, 255, nothing)
-while True:
+camera = cv2.VideoCapture(sys.argv[1])
+while (camera.isOpened()):
 	(grabbed, frame) = camera.read()
-	frame = cv2.resize(frame, (640, 480))
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	thresh = cv2.getTrackbarPos('T', 'videoUI');
-	frame = cv2.threshold(frame, thresh, 255, cv2.THRESH_BINARY)[1]
-	cv2.imshow('image', frame)
-	cv2.waitKey(1)
+	if grabbed == True:
+		cv2.imshow('image', frame)
+		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		hist = cv2.calcHist([gray_frame],[0],None,[256],[0,256])
+		hist,bins = np.histogram(gray_frame.ravel(),256,[0,256])
+		plt.hist(gray_frame.ravel(),256,[0,256])
+		plt.pause(0.05)
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			break
+	else:
+		break
+plt.show()
+camera.release()
