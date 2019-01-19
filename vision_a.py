8307a2c18836252c3bd081fe1e7c80757e098a42
Calvin Hetes <hetes.calvin@gmail.com>
2019-01-19 11:34:12 -0500

    Started working on stuff

--- vision_a.py
+++ vision_a.py
@@ -26,25 +26,42 @@ while (camera.isOpened()):
 		cv2.imshow('blur', blurred)
 		edges = cv2.Canny(blurred,50,150,apertureSize = 3)
 		cv2.imshow('edges', edges)
-		lines = cv2.HoughLines(edges,1,np.pi/180,200)
+		lines = cv2.HoughLines(edges,1,np.pi/180,20)
 		if lines is not None:
-			for rho,theta in lines[0]:
-				a = np.cos(theta)
-				b = np.sin(theta)
-				x0 = a*rho
-				y0 = b*rho
-				x1 = int(x0 + 1000*(-b))
-				y1 = int(y0 + 1000*(a))
-				x2 = int(x0 - 1000*(-b))
-				y2 = int(y0 - 1000*(a))
-				cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+			#go through for lop and find thetas then average them out
+			numoflines = 3
+			totheta = 0
+
+			for count, line in enumerate(lines):
+				if count > numoflines:
+					break
+
+				for rho,theta in line:
+					totheta += theta 
+			totheta2 = 0
+			lc = 0
+			ang90 = theta-.15np.pi
+			for count, line in enumerate(lines):
+				for rho,theta in line:
+					
+			
+			for count, line in enumerate(lines):
+				if count > 10:
+					break
+
+				for rho,theta in line:
+					a = np.cos(theta)
+					b = np.sin(theta)
+					x0 = a*rho
+					y0 = b*rho
+					x1 = int(x0 + 1000*(-b))
+					y1 = int(y0 + 1000*(a))
+					x2 = int(x0 - 1000*(-b))
+					y2 = int(y0 - 1000*(a))
+					cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
 		
 		cv2.imshow('image', frame)
 
-		hist = cv2.calcHist([gray_frame],[0],None,[256],[0,256])
-		hist,bins = np.histogram(gray_frame.ravel(),256,[0,256])
-		plt.hist(gray_frame.ravel(),256,[0,256])
-		plt.pause(0.05)
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			break
 	else:
