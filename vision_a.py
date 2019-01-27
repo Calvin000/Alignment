6d279028ecc18e792c84509d23c962b937b92a47
Zack Garcia-Webb <mlg720noscope@outlook.com>
2019-01-27 16:35:33 -0500

    wdd

--- vision_a.py
+++ vision_a.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 import sys
 from time import sleep
 from matplotlib import pyplot as plt
@@ -10,17 +11,11 @@ cv2.namedWindow("trackbars")
 cv2.createTrackbar("blursize", "trackbars", 1, 10, nothing)
 cv2.createTrackbar("canny", "trackbars", 0, 1, nothing)
 
-m1 = -(cos(theta1)/sin(theta1))
-m2 = -(cos(theta2)/sin(theta2))
-b1 = r1/sin(theta1)
-b2 = r2/sin(theta2)
-
-x = ((b2-b1)/(m1-m2))
-y = ((m1*(b2-b1))/(m1-m2))+b1
-
 camera = cv2.VideoCapture(sys.argv[1])
+playvideo = True
 while (camera.isOpened()):
-	(grabbed, frame) = camera.read()
+	if playvideo:
+		(grabbed, frame) = camera.read()
 
 	blursize = cv2.getTrackbarPos("blursize", "trackbars")*2 + 1
 
@@ -39,47 +34,60 @@ while (camera.isOpened()):
 			#go through for loop and find thetas then average them out
 			numoflines = 3
 			totheta = 0
+			torho1 = 0
 			for count, line in enumerate(lines):
 				if count > numoflines:
 					break
 
 				for rho,theta in line:
 					totheta += theta
+					torho1 += rho
 			avgtheta = totheta / max(numoflines, count)
 			lc = 0
 			totheta2 = 0
+			torho2 = 0
 			ang90 = avgtheta + np.pi/2
 			tol = 15 * (np.pi / 180)
 			for count, line in enumerate(lines):
 				for rho,theta in line:
 					if abs(theta - ang90) < tol:
 						totheta2 += theta
+						torho2 += rho
 						lc += 1
-			print(theta)
 			if lc != 0:
+				avgrho1 = torho1/lc
+				avgrho2 = torho2/lc
 				avgtheta2 = totheta2/lc
-				print(f"Theta 1: {avgtheta * (180 / np.pi)}")
-				print(f"Theta 2: {avgtheta2 * (180 / np.pi)}")
+				m1 = -(math.cos(avgtheta)/math.sin(avgtheta))
+				m2 = -(math.cos(avgtheta2)/math.sin(avgtheta2))
+				b1 = avgrho1/math.sin(avgtheta)
+				b2 = avgrho2/math.sin(avgtheta2)
+
+				x = ((b2-b1)/(m1-m2))
+				y = ((m1*(b2-b1))/(m1-m2))+b1
+
+				print(avgrho1, avgrho2, avgtheta, avgtheta2)
 			for count, line in enumerate(lines):
 				if count > 10:
 					break
 
 				for rho,theta in line:
-					a = np.cos(theta)
-					b = np.sin(theta)
-					x0 = a*rho
-					y0 = b*rho
-					x1 = int(x0 + 1000*(-b))
-					y1 = int(y0 + 1000*(a))
-					x2 = int(x0 - 1000*(-b))
-					y2 = int(y0 - 1000*(a))
-					cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
-		corner1 = (line1 == line2)
-		corner2 = (line3 == line2)
+					if playvideo:
+						a = np.cos(theta)
+						b = np.sin(theta)
+						x0 = a*rho
+						y0 = b*rho
+						x1 = int(x0 + 1000*(-b))
+						y1 = int(y0 + 1000*(a))
+						x2 = int(x0 - 1000*(-b))
+						y2 = int(y0 - 1000*(a))
+						cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
 		cv2.imshow('image', frame)
-
-		if cv2.waitKey(25) & 0xFF == ord('q'):
+		k = cv2.waitKey(25)
+		if k==27:
 			break
+		elif k==ord('p'):
+			playvideo = not playvideo
 	else:
 		break
 plt.show()
